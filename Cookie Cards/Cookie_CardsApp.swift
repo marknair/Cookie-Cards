27b859c5c4a5fe1aaf4a341bf9d970408eef1db6
Mark Nair <marknair@me.com>
2023-11-17 17:54:57 -0600

    Initial Commit

---
+++ Cookie Cards/Cookie_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_CardsApp.swift
+//  Cookie Cards
+//
+//  Created by Mark Nair on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
