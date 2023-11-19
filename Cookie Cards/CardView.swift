e67a08b890e5d73e09eec718c1daa13ba089731b
Mark Nair <marknair@me.com>
2023-11-18 18:50:51 -0600

    Animations

--- Cookie Cards/CardView.swift
+++ Cookie Cards/CardView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct CardView: View {
     // MARK: PROPERTIES
     var card: Card
+    @State private var fadeIn = false
+    @State private var moveDownward = false
+    @State private var moveUpward = false
+    
     var gradient = [Color(.color01), Color(.color02)]
     
     // MARK: BODY
@@ -17,6 +21,7 @@ struct CardView: View {
         ZStack {
             
             Image(card.imageName)
+                .opacity(fadeIn ? 1.0 : 0.0)
                 .offset(y: -90)
             
             VStack {
@@ -31,11 +36,11 @@ struct CardView: View {
                     .italic()
                     .minimumScaleFactor(0.75)
             }
-            .offset(y: -218)
+            .offset(y: moveDownward ? -218 : -300)
             
             Button {
                 print("Tapped")
-                playSound(sound: "sound-chime", type: "mp3")
+                playSound(sound: "happyCookie", type: "mp3")
             } label: {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -52,12 +57,21 @@ struct CardView: View {
                 .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             }
-            .offset(y: 230)
+            .offset(y: moveUpward ? 210 : 300)
         }
         .frame(width: 335, height: 545)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear() {
+            withAnimation(.linear(duration: 1.2)) {
+                fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)) {
+                moveDownward.toggle()
+                moveUpward.toggle()
+            }
+        }
     }
 }
 
